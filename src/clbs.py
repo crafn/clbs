243ad924de00af1378ddcb4146750c5be1ff6a3a
crafn <tuukka.pensala@gmail.com>
2014-10-23 21:15:55 +0300

    Multiplatform support

--- src/clbs.py
+++ src/clbs.py
@@ -1,10 +1,18 @@
-import os, sys
+import platform, os, sys
+
+# Build environment
+# Passed to buildInfo(..) in build.clbs
+class Env:
+    tag= "" # Name supplied by command line
+    os= "" # "windows" or "linux"
+    arch= "x64" # @todo Auto-detect
 
 class Project:
     name= "default"
     src= [] # Paths to source files
     flags= [] # Flags to compiler
     defines= [] # Macro defines to compiler
+    links= []
     targetdir= "."
     compiler= "g++"
     type= "exe" # "exe" or "lib"
@@ -16,7 +24,7 @@ def fail(msg):
 def msg(m):
     print m
 
-def buildProject(cfg, p):
+def buildProject(env, p):
     arg_str= ""
 
     # Compiler flags
@@ -30,8 +38,12 @@ def buildProject(cfg, p):
     # Source files
     arg_str += " " + " ".join(p.src)
 
+    # Links
+    for l in p.links:
+        arg_str += " -l" + l
+
     # Output name
-    arg_str += " -o " + p.targetdir + "/" + p.name + "_" + cfg
+    arg_str += " -o " + p.targetdir + "/" + p.name + "_" + env.tag
 
     # Issue compilation command
     compile_cmd= p.compiler + arg_str
@@ -39,10 +51,6 @@ def buildProject(cfg, p):
     os.system(compile_cmd)
 
 def build(args):
-    if len(args) < 1:
-        fail("Too few arguments")
-    cfg= args[0]
-
     build_file_src= ""
     try:
         file= open("build.clbs", "r")
@@ -52,10 +60,16 @@ def build(args):
 
     # @todo Some restrictions!
     exec(build_file_src)
-    build_info= buildInfo(cfg)
+
+    if len(args) < 1:
+        fail("Too few arguments")
+    env= Env()
+    env.tag= args[0]
+    env.os= platform.system().lower()
+    build_info= buildInfo(env)
     
     if isinstance(build_info, Project):
-        buildProject(cfg, build_info)
+        buildProject(env, build_info)
     else:
         fail("buildInfo returned invalid type: " + type(build_info).__name__)
 
