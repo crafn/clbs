af39aa1d798b2b6b2fd8ef9aa884c7af3764b960
crafn <tuukka.pensala@gmail.com>
2014-10-25 01:39:53 +0300

    Skipping linking if not changes detected

--- src/clbs.py
+++ src/clbs.py
@@ -10,7 +10,6 @@ def run(cmd):
         fail("clbs: build failed")
 
 def buildProject(env, p, cache):
-    log("building " + p.name)
     
     # Find list of outdated source files
     outdated_src= []
@@ -18,45 +17,49 @@ def buildProject(env, p, cache):
         if outdated(src_path, p._compileHash, cache):
             outdated_src.append(src_path)
 
-    # Compile all source files to object files
-    mkDir(p.tempDir)
-    for src_path in outdated_src:
-        arg_str= ""
-        arg_str += " -c" # No linking at this phase
-        for f in p.flags:
-            arg_str += " -" + f
-        for d in p.defines:
-            arg_str += " -D" + d
-        arg_str += " " + src_path
-        arg_str += " -o " + objFilePath(src_path, p)
-
-        compile_cmd= p.compiler + arg_str
-        run(compile_cmd)
-
-        # Add compilation to cache
-        if not p._compileHash in cache.compiles:
-            cache.compiles[p._compileHash]= { 
-                    "srcBuildTimes": {},
-                    "srcDeps": []
-             }
-        cache.compiles[p._compileHash]["srcBuildTimes"][src_path]= \
-            modTime(src_path)
-
-    # Link object files
-    arg_str= ""
-    for s in p.src:
-        arg_str += " " + objFilePath(s, p)
-    for l in p.links:
-        arg_str += " -l" + l
-
-    if p.type == "exe":
-        arg_str += " -o " + targetPath(p)
-        run(p.compiler + arg_str)
-    elif p.type == "lib":
-        arg_str= " rcs " + targetPath(p) + " " + arg_str
-        run(p.archiver + arg_str)
+    if len(outdated_src) == 0:
+        log("unchanged " + p.name)
     else:
-        fail("Unsupported project type: " + p.type)
+        log("building " + p.name)
+        # Compile all source files to object files
+        mkDir(p.tempDir)
+        for src_path in outdated_src:
+            arg_str= ""
+            arg_str += " -c" # No linking at this phase
+            for f in p.flags:
+                arg_str += " -" + f
+            for d in p.defines:
+                arg_str += " -D" + d
+            arg_str += " " + src_path
+            arg_str += " -o " + objFilePath(src_path, p)
+
+            compile_cmd= p.compiler + arg_str
+            run(compile_cmd)
+
+            # Add compilation to cache
+            if not p._compileHash in cache.compiles:
+                cache.compiles[p._compileHash]= { 
+                        "srcBuildTimes": {},
+                        "srcDeps": []
+                 }
+            cache.compiles[p._compileHash]["srcBuildTimes"][src_path]= \
+                modTime(src_path)
+
+        # Link object files
+        arg_str= ""
+        for s in p.src:
+            arg_str += " " + objFilePath(s, p)
+        for l in p.links:
+            arg_str += " -l" + l
+
+        if p.type == "exe":
+            arg_str += " -o " + targetPath(p)
+            run(p.compiler + arg_str)
+        elif p.type == "lib":
+            arg_str= " rcs " + targetPath(p) + " " + arg_str
+            run(p.archiver + arg_str)
+        else:
+            fail("Unsupported project type: " + p.type)
 
 def cleanProject(env, p, cache):
     log("cleaning " + p.name)
